401afea438f9971a213c354ed07a228995d6a545
kodeninj <femifadiya.segun@gmail.com>
2024-03-14 10:10:30 +0100

    updated the popular categories sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import ValuesPanel from "@/components/ValuesPanel";
 import React from "react";
-import Data from "@/Data";
+import Data from "@/Data/popularProducts";
+import Popular from "@/components/PopularCategories";
 const page = () => {
 	return (
 		<div className="w-full min-h-[100vh]">
 			<Hero />
 			<ValuesPanel />
+			<Popular/>
 		</div>
 	);
 };
---
+++ src/components/PopularCategories/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "../Headers";
+import popularData from "@/Data/popularProducts";
+import Image from "next/image";
+const Popular = () => {
+	return (
+		<div className="lg:px-[100px] md:px-[40px] px-[20px] py-[8px]">
+			<Header
+				title="Popular Categories"
+				view_all="View all"
+			/>
+			<div className="flex flex-wrap lg:justify-between justify-center gap-[24px] items-center text-center mt-[32px]">
+				{popularData.map((each): JSX.Element => {
+					return (
+						<div
+							key={each.id}
+							className="border-[1px] py-4 border-[var(--bordercolor)] rounded-lg hover:border-[var(--hard-Primary)] cursor-pointer transition-all ease-linear hover:shadow-lg">
+							<Image
+								src={each.imgSrc}
+								alt={each.title}
+								width={190}
+							/>
+							<p className="mt-[24px] text-[18px] font-medium">{each.title}</p>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default Popular;
